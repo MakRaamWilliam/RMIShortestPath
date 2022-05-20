2b4e73c140e272096a1901c7b8ceeb2ebcfac3ff
MakRaamWilliam <makrm.wilyam99@gmail.com>
2022-05-20 20:49:25 +0200

    remove interface

--- RMIRegistery/src/rmi/registery/GraphService.java
+++
@@ -1,9 +0,0 @@
-package rmi.registery;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface GraphService extends Remote {
-	public String excuteBatchOperations(String batch) throws RemoteException;
-	public String getCurrentGraph() throws RemoteException;
-}
